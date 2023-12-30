0e5392a3b244202d116b400109c7aed0704e106b
capashaa <130944764+capashaa@users.noreply.github.com>
2023-12-30 23:29:29 +0100

    Minor

--- EEditor/Properties/Resources.Designer.cs
+++ EEditor/Properties/Resources.Designer.cs
@@ -131,6 +131,16 @@ namespace EEditor.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap blocks_shape {
+            get {
+                object obj = ResourceManager.GetObject("blocks_shape", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] blocktext {
